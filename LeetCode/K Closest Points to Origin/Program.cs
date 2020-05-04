21cf2325ab1b5dea24bebed4740cd12a92823d6a
ShawnPan <1329412398@qq.com>
2020-05-04 16:14:03 +0800

    添加题目序号

---
+++ LeetCode/K Closest Points to Origin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace K_Closest_Points_to_Origin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
