ff5277b3b08dabff66ef74eca24a8b9b98920064
ShawnPan <1329412398@qq.com>
2020-08-09 00:07:07 +0800

    32双周赛

---
+++ Weekly Contest/Weekly Contest/1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] x = { 2, 3, 4, 7, 11 };
+            Console.WriteLine((int) FindKthPositive(x, 5));
+        }
+
+        public static int FindKthPositive(int[] arr, int k)
+        {
+            if (arr.Length == 0)
+            {
+                return k;
+            }
+
+            int count = 0;
+
+            int i = 0, j = 1;
+            while (i < arr.Length)
+            {
+                if (arr[i] == j)
+                {
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    j++;
+                    count++;
+                }
+
+                if (count == k)
+                {
+                    return j - 1;
+                }
+            }
+
+            return k - count + j;
+        }
+    }
+}
---
+++ Weekly Contest/Weekly Contest/2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string x1 = "input", x2 = "output";
+            string y1 = "abc", y2 = "bcd";
+            string z1 = "aab", z2 = "bbb";
+            Console.WriteLine(CanConverString(x1,x2,9));
+            Console.WriteLine(CanConverString(y1,y2,10));
+            Console.WriteLine(CanConverString(z1,z2,27));
+        }
+
+        public static List<char> circle = new List<char>();
+        public static bool CanConverString(string s, string t, int k)
+        {
+            int times = 0;
+            int count = 0;
+
+            int i = 0, j = 0;
+
+            while (i<s.Length)
+            {
+                
+
+            }
+
+
+
+            return true;
+        }
+
+
+        public static void CreateCircle()
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                circle.Add('a' + i);
+            }
+        }
+    }
+}
