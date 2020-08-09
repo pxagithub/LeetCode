223b3f5d284bbbc1280e72b103e7bb65ecd977a7
ShawnPan <1329412398@qq.com>
2020-08-09 13:17:03 +0800

    201周赛

---
+++ Weekly Contest/Weekly Contest/201-1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _201_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "leEeetcode", s2 = "abBAcC", s3 = "s";
+            Console.WriteLine(MakeGood(s1));
+            Console.WriteLine(MakeGood(s2));
+            Console.WriteLine(MakeGood(s3));
+
+        }
+
+        public static string MakeGood(string s)
+        {
+            int i = 0, j = 1;
+            while (j<s.Length)
+            {
+                if ((s[i]==char.ToLower(s[j])&&s[i]!=s[j])|| (s[j] == char.ToLower(s[i]) && s[i] != s[j]))
+                {
+                    s = s.Remove(j, 1);
+                    s = s.Remove(i, 1);
+                    i = 0;
+                    j = 1;
+                }
+                else
+                {
+                    i++;
+                    j++;
+                }
+            }
+
+            return s;
+        }
+
+        
+    }
+}
---
+++ Weekly Contest/Weekly Contest/201-2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace _201_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindKthBit(3, 2));//1
+            Console.WriteLine(FindKthBit(4, 12));//0
+            Console.WriteLine(FindKthBit(1, 1));//0
+            Console.WriteLine(FindKthBit(2, 3));//1
+            Console.WriteLine(FindKthBit(6,57));//0
+
+        }
+
+        public static char FindKthBit(int n, int k)
+        {
+            int[] length=new int[n];
+            length[0] = 1;
+
+            for (int i = 1; i < n; i++)
+            {
+                length[i] = 2 * length[i - 1] + 1;
+            }
+
+            return Recur(n, k, length);
+
+        }
+
+        public static char Recur(int n, int k, int[] length)
+        {
+            if (n == 1||k==1)
+            {
+                return '0';
+            }
+
+            if (k <= length[n - 1] / 2)
+            {
+                return Recur(n - 1, k,length);
+            }
+
+            if (k == (length[n - 1] + 1) / 2)
+            {
+                return '1';
+            }
+
+            else
+            {
+                return Recur(n - 1, 2 * length[n - 2] - k + 2,length)=='0'?'1':'0';
+            }
+        }
+    }
+}
