53ccde7a1cbee9c8889b2ac3f6f376c77c97091c
ShawnPan <1329412398@qq.com>
2020-09-13 17:39:02 +0800

    完成206周赛

---
+++ Weekly Contest/Weekly Contest/206-1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _206_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] x =
+            {
+                new[] {1,0,0},
+                new[]{0,0,1},
+                new[]{1,0,0}
+            };
+            Console.WriteLine(NumSpecial(x));
+            
+        }
+
+        public static int NumSpecial(int[][] mat)
+        {
+            int res = 0;
+            for (int i = 0; i < mat.Length; i++)
+            {
+                for (int j = 0; j < mat[0].Length; j++)
+                {
+
+                    if (mat[i][j] == 1 && IsSpecial(mat, i, j)) 
+                    {
+                        res++;
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        public static bool IsSpecial(int[][] mat, int row, int column)
+        {
+            for (int i = 0; i < mat.Length; i++)
+            {
+                if (i != row && mat[i][column] == 1) 
+                {
+                    return false;
+                }
+            }
+
+            for (int i = 0; i < mat[0].Length; i++)
+            {
+                if (i != column && mat[row][i] == 1) 
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Weekly Contest/Weekly Contest/206-2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _206_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] xPreferences =
+            {
+                new []{1,3,2},//0
+                new []{2,3,0},//1
+                new []{1,3,0},//2
+                new []{0,2,1}//3
+            };
+            int[][] xPairs =
+            {
+                new[] {1,3},
+                new[] {0,2}
+            };
+
+            Console.WriteLine(UnhappyFriends(4,xPreferences,xPairs));
+        }
+
+        public static int UnhappyFriends(int n, int[][] preferences, int[][] pairs)
+        {
+            int res = 0;
+            Dictionary<int, int> pairsDictionary = new Dictionary<int, int>();
+
+            for (int i = 0; i < pairs.Length; i++)
+            {
+                pairsDictionary.Add(pairs[i][0], pairs[i][1]);
+                pairsDictionary.Add(pairs[i][1], pairs[i][0]);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int pairIndex_i = Array.IndexOf(preferences[i],pairsDictionary[i]);
+                for (int j = 0; j < n; j++)
+                {
+                    if (i!=j)
+                    {
+                        int pairIndex_j = Array.IndexOf(preferences[j], pairsDictionary[j]);
+                        int friendIndex_i = Array.IndexOf(preferences[j], i);
+                        int friendIndex_j = Array.IndexOf(preferences[i], j);
+                        if (friendIndex_j<pairIndex_i&&friendIndex_i<pairIndex_j)
+                        {
+                            res++;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
