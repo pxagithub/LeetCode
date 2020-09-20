5a5b98e4b618e81c3e1ddb4b41638254e20d6d91
ShawnPan <1329412398@qq.com>
2020-09-20 12:53:09 +0800

    完成每日一题78

---
+++ DaliyQuestion/20--78.Subsets/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _20__78.Subsets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var subset in Subsets(new[] { 1, 2, 3 }))
+            {
+                Console.WriteLine("["+string.Join(",",subset)+"]");
+            }
+        }
+        public static IList<IList<int>> Subsets(int[] nums)
+        {
+            IList<IList<int>> res = new List<IList<int>>();
+            res.Add(new List<int>());
+            if (nums.Length==0)
+            {
+                return res;
+            }
+            BackTracking(nums,0,new List<int>() , res);
+            return res;
+
+        }
+
+        public static void BackTracking(int[] nums, int start, List<int> path, IList<IList<int>> res)
+        {
+            if (start==nums.Length)
+            {
+                return;
+            }
+
+            for (int i = start; i < nums.Length; i++)
+            {
+                path.Add(nums[i]);
+                if (!res.Contains(path))
+                {
+                    res.Add(new List<int>(path));
+                }
+
+                BackTracking(nums, i + 1, path, res);
+
+                path.Remove(nums[i]);
+            }
+        }
+    }
+}
