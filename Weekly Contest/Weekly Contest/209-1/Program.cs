b943f45eded2fd0fc3f911181bbcca3463a9db02
ShawnPan <1329412398@qq.com>
2020-10-04 21:06:13 +0800

    完成209周赛

---
+++ Weekly Contest/Weekly Contest/209-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _209_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SpecialArray(new []{ 0, 4, 3, 0, 4 }));
+        }
+
+        public static int SpecialArray(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < nums.Length; i++)
+                {
+                    if (i <= nums[j]) count++;
+                }
+                if (count == i) return i;
+            }
+            return -1;
+
+        }
+    }
+}
