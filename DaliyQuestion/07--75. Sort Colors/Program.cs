21d155117c8183b48c32aa51fbe97c91ee280dd2
ShawnPan <1329412398@qq.com>
2020-10-11 10:51:33 +0800

    完成每日一题75

---
+++ DaliyQuestion/07--75. Sort Colors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07__75._Sort_Colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {2, 0, 2, 1, 1, 0};
+            SortColors(nums);
+            Console.WriteLine(string.Join(",",nums));
+        }
+
+        public static void SortColors(int[] nums)
+        {
+            int n = nums.Length;
+            int i = 0, j = n - 1;
+            int count = 0;
+            while (i<=j)
+            {
+                
+            }
+
+        }
+    }
+}
