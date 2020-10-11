ee1135ae5864f3287aad001f004b16955cff84da
ShawnPan <1329412398@qq.com>
2020-10-11 22:08:43 +0800

    210周赛

---
+++ Weekly Contest/Weekly Contest/210-2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _210_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] x =
+            {
+                new[] {0, 1},
+                new[] {0, 3},
+                new[] {1, 2},
+                new[] {1, 3}
+            };
+            Console.WriteLine(MaximalNetworkRank(4,x));
+        }
+
+        public static int MaximalNetworkRank(int n, int[][] roads)
+        {
+            Dictionary<int, List<int>> dic = new Dictionary<int, List<int>>();
+            int max = 0;
+            foreach (var point in roads)
+            {
+                if (!dic.ContainsKey(point[0]))
+                {
+                    dic.Add(point[0],new List<int>());
+                    dic[point[0]].Add(point[1]);
+                }
+                else
+                {
+                    dic[point[0]].Add(point[1]);
+                }
+                if (!dic.ContainsKey(point[1]))
+                {
+                    dic.Add(point[1], new List<int>(point[0]));
+                    dic[point[1]].Add(point[0]);
+                }
+                else
+                {
+                    dic[point[1]].Add(point[0]);
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (dic.ContainsKey(i))
+                {
+                    for (int j = i+1; j < n; j++)
+                    {
+                        if (dic.ContainsKey(j))
+                        {
+                            int temp = dic[i].Count + dic[j].Count;
+                            if (dic[i].Contains(j))
+                            {
+                                temp--;
+                            }
+
+                            max = max > temp ? max : temp;
+                        }
+                
+                    }
+                }
+            }
+
+            return max;
+
+        }
+
+
+
+        
+    }
+}
---
+++ Weekly Contest/Weekly Contest/210-3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _210_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckPalindromeFormation("ulacfd", "jizalu"));
+        }
+
+        public static bool CheckPalindromeFormation(string a, string b)
+        {
+            int n = a.Length;
+            if (n==1)
+            {
+                return true;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                string temp1 = a.Substring(0, i) + b.Substring(i, n - i);
+                string temp2=new string(temp1.ToCharArray().Reverse().ToArray());
+                if (temp1==temp2)
+                {
+                    return true;
+                }
+
+                temp1 = b.Substring(0, i) + a.Substring(i, n - i);
+                temp2= new string(temp1.ToCharArray().Reverse().ToArray());
+                if (temp1==temp2)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
