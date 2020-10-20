e0a6ee4a1b6f1f1651c3cf53888cca9f8bae380e
ShawnPan <1329412398@qq.com>
2020-10-20 16:24:34 +0800

    完成每日一题143

---
+++ DaliyQuestion/20--143. Reorder List/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using DataStructLibrary;
+
+namespace _20__143._Reorder_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode head=new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                    }
+                }
+            };
+            ReorderList(head);
+            Console.WriteLine();
+        }
+
+        public static void ReorderList(ListNode head)
+        {
+            if (head==null)
+            {
+                return;
+            }
+            List<ListNode> list = new List<ListNode>();
+            while (head!=null)
+            {
+                list.Add(head);
+                head = head.next;
+            }
+
+            int i = 0, j = list.Count - 1;
+            while (i<=j)
+            {
+                list[i].next = list[j];
+                list[j].next = list[i + 1];
+                i++;
+                j--;
+            }
+
+            list[i].next = null;
+        }
+    }
+}
