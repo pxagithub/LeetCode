79e8ff98770859433c37379ae1af9a61dfe31b1c
ShawnPan <1329412398@qq.com>
2020-11-15 17:28:39 +0800

    双周赛39

---
+++ Weekly Contest/BiWeekly Contest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BiWeekly_Contest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
