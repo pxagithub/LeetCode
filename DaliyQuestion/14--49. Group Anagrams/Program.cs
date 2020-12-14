47be4d033de6172af3dfc120300d8065db344754
ShawnPan <1329412398@qq.com>
2020-12-14 16:37:05 +0800

    完成每日一题49

---
+++ DaliyQuestion/14--49. Group Anagrams/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14__49._Group_Anagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<IList<string>> a=GroupAnagrams(new []{ "eat", "tea", "tan", "ate", "nat", "bat" });
+            Console.WriteLine();
+        }
+        public static IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, List<string>> dic = new Dictionary<string, List<string>>();
+            for (int i = 0; i < strs.Length; i++)
+            {
+                char[] temp = strs[i].ToCharArray();
+                Array.Sort(temp);
+                string s=new string(temp);
+                if (dic.ContainsKey(s))
+                {
+                    dic[s].Add(strs[i]);
+                }
+                else
+                {
+                    dic.Add(s,new List<string>(){strs[i]});
+                }
+            }
+
+            return new List<IList<string>>(dic.Values);
+        }
+    }
+}
