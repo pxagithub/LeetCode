ecce625d6ba429cdd08ce070e261da39e4b824b3
ShawnPan <1329412398@qq.com>
2020-12-24 15:12:45 +0800

    完成每日一题135

---
+++ DaliyQuestion/24--135.Candy/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Versioning;
+
+namespace _24__135.Candy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Candy(new[] {1, 0, 2}));//5
+            Console.WriteLine(Candy(new[] {1, 2, 2}));//4
+            Console.WriteLine(Candy(new[] {1, 2, 87, 87, 87, 2, 1}));//13
+            Console.WriteLine(Candy(new[] {1, 2, 3, 3, 2, 4, 5, 6, 1, 2, 0}));//22
+        }
+        public static int Candy(int[] ratings)
+        {
+            int n = ratings.Length;
+            int[] res = new int[n];
+            for (int i = 1; i < n; i++)
+            {
+                int pre = i - 1;
+                if (ratings[i] > ratings[pre])
+                {
+                    res[i] = Math.Max(res[i], res[pre] + 1);
+                }
+            }
+
+            for (int i = n-2; i >= 0; i--)
+            {
+                int next = i + 1;
+                if (ratings[i]>ratings[next])
+                {
+                    res[i] = Math.Max(res[i], res[next] + 1);
+                }
+            }
+            
+            return res.Sum()+n;
+
+
+        }
+    }
+}
