ded5ebc2c7e123fb9186252931ed612f2a8717f7
ShawnPan <1329412398@qq.com>
2021-05-09 14:28:44 +0800

    240周赛

---
+++ Weekly Contest/Weekly Contest/238-1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _238_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumBase(34,6));
+            Console.WriteLine(SumBase(42,2));
+        }
+
+        public static int SumBase(int n, int k)
+        {
+            int res = 0;
+            while (n > 0)
+            {
+                if (n >= k)
+                {
+                    int temp = n / k;
+                    while (temp>k)
+                    {
+                        temp /= k;
+                    }
+                    if (temp<k)
+                    {
+                        res += temp;
+                    }
+                    
+
+                    
+
+                }
+                else
+                {
+                    res += n;
+                    n = 0;
+                }
+                
+            }
+            return res;
+
+        }
+    }
+}
---
+++ Weekly Contest/Weekly Contest/240-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _240_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x1 = {55, 30, 5, 4, 2};
+            int[] x2 = {100, 20, 10, 10, 5};
+            Console.WriteLine(MaxDistance(x1,x2));
+        }
+        public static int MaxDistance(int[] nums1, int[] nums2)
+        {
+            int i = 0;
+            int res = 0;
+            while (i < nums1.Length)
+            {
+                int left = 0, right = nums2.Length-1;
+                int mid = (left + right) / 2;
+                while (nums1[i]>nums2[mid])
+                {
+                    mid = (mid + left) / 2;
+                }
+
+                while (mid+1<nums2.Length&&nums2[mid+1]>=nums1[i])
+                {
+                    mid++;
+                }
+
+                res = Math.Max(res, mid - i);
+
+                i++;
+            }
+
+            return res;
+
+        }
+    }
+}
